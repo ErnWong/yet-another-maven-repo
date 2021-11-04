864ea09bfb60c8473aeb198021140ad186c0d2fa
Ernest Wong <ewon521@gmail.com>
2021-11-04 23:43:24 +1300

    cough

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,7 +2,7 @@ import javax.swing.text.html.HTMLDocument;
 import java.awt.*;
 
 public class Student extends Person {
-    private final HTMLDocument testScore;
+    public final HTMLDocument testScore;
 
     public Student(String firstName, String lastName, int age, Color eyeColour, Color hairColour, HTMLDocument testScore) {
         super(firstName, lastName, age, eyeColour, hairColour);
